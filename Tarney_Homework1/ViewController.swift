83fa3826355738ab356339a9657ec236283c0d67
1amBulletproof <brandon.tarney@gmail.com>
2018-02-11 21:45:56 -0500

    Initial Commit

---
+++ Tarney_Homework1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tarney_Homework1
+//
+//  Created by Brandon Tarney on 2/11/18.
+//  Copyright © 2018 Johns Hopkins University. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
